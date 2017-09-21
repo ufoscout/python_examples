30683c939d7212ca4a3797c5ac6b1ad75d849ce2
ufoscout <ufoscout@gmail.com>
2017-09-21 21:23:55 +0200

    Some examples

---
+++ 001_HelloWorld/string_interpoòation.py
@@ -0,0 +1,10 @@
+first = 123
+second = 333
+third = 999
+
+print "first is %s" % (first)
+print "first is %s second is" % first, second
+print "first*2 is %s " % first*2
+print "(first*2) is %s " % (first*2)
+print "first is %s, second is %s, third is %s" % (first, second, third)
+print "sum of first + second + third is %s" %(first + second + third)
