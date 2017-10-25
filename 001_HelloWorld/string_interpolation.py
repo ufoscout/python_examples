efeab0e4c220e76ef4131ff6f76c4c09ee99f4b3
ufoscout <ufoscout@gmail.com>
2017-10-25 20:20:01 +0200

    Add simple docker-compose file

--- 001_HelloWorld/string_interpolation.py
+++ 001_HelloWorld/string_interpolation.py
@@ -4,7 +4,7 @@ third = 999
 
 print "first is %s" % (first)
 print "first is %s second is" % first, second
-print "first*2 is %s " % first*2
+print "first*2 is %s " % first*2 #this one is tricky!!!
 print "(first*2) is %s " % (first*2)
 print "first is %s, second is %s, third is %s" % (first, second, third)
 print "sum of first + second + third is %s" %(first + second + third)
